b6da377f882a9950f6e432015c4bf15a76c8b484
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2024-09-08 23:23:30 +0530

    fix

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -14,7 +14,7 @@ export const Appbar = () => {
   const [menuExpand, setMenuExpand] = useState<boolean>(false);
 
   return (
-    <nav className="fixed w-full z-20 top-0 start-0 border-b  border-gray-600 bg-black lg:bg-red-500 md:bg-green-600 sm:bg-blue-700">
+    <nav className="fixed w-full z-20 top-0 start-0 border-b  border-gray-600 bg-black">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="/"
